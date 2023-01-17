1cf967463b14063aac8fbf2ac5ca8d3364c30dbb
spli <lishoupeng@zhihu.com>
2023-01-17 16:11:46 +0800

    feat: curator

---
+++ src/main/java/com/lishoupeng/curator/ha/LeaderDriver.java
@@ -0,0 +1,20 @@
+package com.lishoupeng.curator.ha;
+
+/**
+ * @Description LeaderDriver.
+ * @Author lishoupeng
+ * @Date 2023/1/17 11:57
+ */
+public interface LeaderDriver {
+
+    boolean hasLeadership();
+
+    void joinElection();
+
+    void giveUpElection();
+
+    void rejoinElection();
+
+    boolean isActive();
+
+}
